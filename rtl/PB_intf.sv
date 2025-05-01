eacd8e41763d4776281a24672b86bb6763dafb1e
emlin4 <162045953+emlin4@users.noreply.github.com>
2025-05-01 02:33:14 -0500

    commented Pb_intf

--- rtl/PB_intf.sv
+++ rtl/PB_intf.sv
@@ -1,14 +1,17 @@
 `default_nettype none
 module PB_intf(clk, rst_n, tgglMd, scale, setting);
 
+    // declare input and output signals
     input logic clk, rst_n, tgglMd;
     output logic [2:0] scale;
 
     logic released;
     output logic [1:0] setting;
 
+    // call the Push button release 
     PB_release PB1(.clk(clk), .rst_n(rst_n), .PB(tgglMd), .released(released));
 
+    // flip flop for the setting, only increment setting if released goes high
     always_ff @(posedge clk, negedge rst_n) begin
         if (!rst_n) begin
             setting <= 2'b10;
@@ -17,6 +20,7 @@ module PB_intf(clk, rst_n, tgglMd, scale, setting);
         end
     end
 
+    // assign scale based on the setting value
     assign scale = (setting == 2'b00) ? 3'b000 :
                   (setting == 2'b01) ? 3'b011 :
                   (setting == 2'b10) ? 3'b101 :
